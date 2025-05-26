5accc72d2948fb4075b7cc6d94d96a7cf5c62da7
RafaelAsafe <asafe.ferr@gmail.com>
2025-05-26 00:19:20 -0300

    :tada: feat(query_database): implementando filtros na query

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,7 +3,7 @@ from decouple import config
 from src.notion_alchemy.client import NotionClient
 from src.notion_alchemy.models import NotionDatabaseModel
 
-API_KEY= config('NOTION_TOKEN')
+API_KEY = config('NOTION_TOKEN')
 PAGE_ID = config('PAGE_ID')
 DATABASE_ID = config('DATABASE_ID')
 
@@ -17,6 +17,9 @@ def test_request_page():
 def test_query_database():
     client = NotionClient(API_KEY)
     pagina = client.get_database(DATABASE_ID)
+
     database = NotionDatabaseModel.from_notion(page=pagina)
-    resultado = client.query_database(database)
+    resultado = client.query_database(database,database.tags.contains('Casa'))
     assert resultado is not None
+
+# 
